6e6bfbd3bcab8fb83b2851c06d7c06e5ed37f148
jhozera <152558072+jhozera@users.noreply.github.com>
2025-01-07 23:33:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-class MensagemPokemon  {
-    constructor (mensagem1, mensagem2, mensagem3) { 
+class MensagemRobo {
+    constructor(mensagem1, mensagem2, mensagem3){
         this.mensagem1 = mensagem1;
         this.mensagem2 = mensagem2;
-        this.mensagem3 = mensagem3;
- }
+        this.mensagem3 = mensagem3; 
+    } 
+
 }
 
-class MensagemPokemon1 extends MensagemPokemon {
-    constructor (mensagem1, mensagem2, mensagem3, mensagem4) {
-        super(mensagem1, mensagem2, mensagem3 );
-        this.mensagem4 = mensagem4;
+class MensagemRobo2 extends MensagemRobo{
+    constructor(mensagem1, mensagem2, mensagem3, mensagemespecial) {
+        super(mensagem1, mensagem2, mensagem3);
+        this.mensagemespecial = mensagemespecial;
+    }
+}    
+
+const mensagem = new MensagemRobo2 ('Olá, bem vindo a nossa central de renegociação', 'Venha conhecer nossas opções especias para sua renegociação', 'Um de nossos especialistas vai passar todas proposta disponiveis', 'Emitir boleto');
+
+console.log(mensagem);
+document.getElementById("Resultado").innerHTML ="<p>Resultado:</p>" + 
+"<p>" + mensagem.mensagem1 + "</p>" + 
+"<p>" + mensagem.mensagem2 + "</p>" + 
+"<p>" + mensagem.mensagemespecial + "</p>" + 
+"<p>" + mensagem.mensagem3 + "</p>";
+
+document.getElementById("button").onclick = function () {
+    let Resultado = document.getElementById("Resultado")
+
+    if(Resultado.style.display == "none"){
+        Resultado.style.display = "block";
+
+    } else {
+        Resultado.style.display = "none";
+    }
+      
+}
+
+
+
+async function starwars () {
+    const wars = await fetch ('https://swapi.py4e.com/api/people/?search').then((res) => res.json())
+    console.log(wars)
+
+    
+    try {
+        
+        if (wars.results.length > 0) {
+            const personagem = wars.results[0];
+            document.getElementById("result").innerHTML = `
+                <h2>${personagem.name}</h2>
+                <p><strong>Altura:</strong> ${personagem.height} cm</p>
+            `;
+        } else {
+            document.getElementById("result").innerHTML = `<p>Personagem não encontrado!</p>`;
+        }
+    } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+        document.getElementById("result").innerHTML = `<p>Ocorreu um erro ao buscar os dados.</p>`;
     }
 }
 
-
-
-const MensagemPokemon12 = new MensagemPokemon1 ('Parabens pelo escolha', 'otima opçao de pokemon', 'a melhor escolha de pokemon','parabens pelo pokemon');
-
-
-
-document.getElementById("pokemonmensagem").innerHTML = `<p>${MensagemPokemon12.mensagem3}</p>`;
-
-
-
-function  buscarpokemon() { 
-
-const nomePokemon = document.getElementById("pokemonName").value.toLowerCase(); // Nome do Pokémon em minúsculas
-            const url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`; // URL da API para buscar o Pokémon
-
-fetch(url)
-
-
-    .then(function(pokedexdata){
-        return pokedexdata.json(); 
-    })
-    .then(function(jsondata){
-        console.log(jsondata);
-    
-
-        document.getElementById("pokemonInfo").innerHTML = `
-        <h2>${jsondata.name.toUpperCase()}</h2>
-        <img src="${jsondata.sprites.front_default}" alt="${jsondata.name}" />
-        <p><strong>Altura:</strong> ${jsondata.height / 10} metros</p>
-        <p><strong>Peso:</strong> ${jsondata.weight / 10} kg</p>
-        <p><strong>Tipos:</strong> ${jsondata.types.map(t => t.type.name).join(", ")}</p>
-    `;
-})
-
-
-    .catch (function (e) {
-        console.log('deu erro');
-    });
- }
+starwars()
